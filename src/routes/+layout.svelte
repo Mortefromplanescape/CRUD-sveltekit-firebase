0d216c02425c9eb3bba88a655e7a3f40de81d267
Mortefromplanescape <boreich89@gmail.com>
2024-09-11 16:20:23 +0300

    Auth, design

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<div class="mainContainer">
+    <slot/>
+</div>
+
+<style>
+    .mainContainer {
+        min-height: 100vh;
+        background: linear-gradient(to right, #002804, #004600);
+        color: white;
+        position: relative;
+        display: flex;
+        flex-direction: column;
+    }
+</style>
